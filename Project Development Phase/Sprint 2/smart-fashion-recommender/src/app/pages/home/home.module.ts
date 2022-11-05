a2dfae230aa9d5891be73e328ee604ae95297b97
devendran <devendranvemula@gmail.com>
2022-11-05 07:31:35 +0530

    new landing banner added

--- Project Development Phase/Sprint 2/smart-fashion-recommender/src/app/pages/home/home.module.ts
+++ Project Development Phase/Sprint 2/smart-fashion-recommender/src/app/pages/home/home.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LandingBannerComponent } from './landing-banner/landing-banner.component';
+import { LandingContainerComponent } from './landing-container/landing-container.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     LandingBannerComponent,
+    LandingContainerComponent,
   ],
   imports: [
     CommonModule,
