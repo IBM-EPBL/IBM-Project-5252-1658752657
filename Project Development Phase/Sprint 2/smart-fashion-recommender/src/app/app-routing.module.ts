d4cf85432bf58c4cbe8174c8d04a50baeaaa90bb
devendran <devendranvemula@gmail.com>
2022-11-05 17:35:43 +0530

    signup page

--- Project Development Phase/Sprint 2/smart-fashion-recommender/src/app/app-routing.module.ts
+++ Project Development Phase/Sprint 2/smart-fashion-recommender/src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
     ],
   },
   { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
+  { path: 'signup', loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupModule) },
 ];
 
 @NgModule({
