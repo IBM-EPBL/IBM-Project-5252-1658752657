6239e2b7c8beb492f322ba4b12676e57d675ba90
devendran <devendranvemula@gmail.com>
2022-11-05 10:53:18 +0530

    new product card

--- Project Development Phase/Sprint 2/smart-fashion-recommender/src/app/pages/home/home.module.ts
+++ Project Development Phase/Sprint 2/smart-fashion-recommender/src/app/pages/home/home.module.ts
@@ -6,17 +6,21 @@ import { HomeComponent } from './home.component';
 import { LandingContainerComponent } from './landing-container/landing-container.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { HomeSliderComponent } from './home-slider/home-slider.component';
+import { TrendingSectionComponent } from './trending-section/trending-section.component';
+import { ProductCardModule } from 'src/app/components/product-card/product-card.module';
 
 @NgModule({
   declarations: [
     HomeComponent,
     LandingContainerComponent,
     HomeSliderComponent,
+    TrendingSectionComponent,
   ],
   imports: [
     CommonModule,
     HomeRoutingModule,
     CarouselModule,
+    ProductCardModule
   ]
 })
 export class HomeModule { }
