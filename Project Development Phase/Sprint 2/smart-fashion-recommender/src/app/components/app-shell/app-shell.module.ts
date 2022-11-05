b1f251aca93a0defbb939f5ea79b04bf3cd54fe1
devendran <devendranvemula@gmail.com>
2022-11-05 15:06:01 +0530

    footer added

--- Project Development Phase/Sprint 2/smart-fashion-recommender/src/app/components/app-shell/app-shell.module.ts
+++ Project Development Phase/Sprint 2/smart-fashion-recommender/src/app/components/app-shell/app-shell.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AppShellComponent } from './app-shell.component';
 import { RouterModule } from '@angular/router';
 import { NavBarModule } from '../nav-bar/nav-bar.module';
+import { FooterModule } from '../footer/footer.module';
 
 
 
@@ -11,7 +12,8 @@ import { NavBarModule } from '../nav-bar/nav-bar.module';
   imports: [
     CommonModule,
     RouterModule,
-    NavBarModule
+    NavBarModule,
+    FooterModule
   ],
   exports: [
     AppShellComponent
